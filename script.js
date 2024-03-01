29036d8e1a09d6367b6cb6a9e02c3bc081851004
Vadim Pusceddu <hankwodo@gmail.com>
2024-03-01 16:21:22 +0000

    su

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ function toggleMenu() {
   // Call the function on page load and resize
   window.addEventListener('load', toggleMenu);
   window.addEventListener('resize', toggleMenu);
+
+//   back to top 
+
+function toggleMenu() {
+    var menu = document.querySelector('.mobilex');
+    if (window.innerWidth < 800) {
+      menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
+  }
+  
+  // Function to scroll to the top of the page
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  // Call the function on page load and resize
+  window.addEventListener('load', toggleMenu);
+  window.addEventListener('resize', toggleMenu);
+  
+  // Show back-to-top button when scrolling
+  window.addEventListener('scroll', function() {
+    var backToTopBtn = document.getElementById('backToTopBtn');
+    if (window.scrollY > 300) {
+      backToTopBtn.style.display = 'block';
+    } else {
+      backToTopBtn.style.display = 'none';
+    }
+  });
